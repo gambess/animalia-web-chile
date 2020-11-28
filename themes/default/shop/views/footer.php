0f51b2a67e0481d1473a0dba6a6504ec173e738f
G4MB3SS <razielvalle@gambess.com>
2020-11-28 17:05:38 -0300

    Raziel Valle Miranda: Añadiendo Footer ya creado

--- themes/default/shop/views/footer.php
+++ themes/default/shop/views/footer.php
@@ -1,83 +1,83 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <section id="footer" class="footer_area">
-            <div class="footer_subscribe pt-80">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-lg-6">
-                            <div class="subscribe_title mt-45">
-                                <h4 class="title">Subscribe Our Newsletter</h4>
-                                <p>To recieve monthly updates</p>
-                            </div> 
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="subscribe_form mt-50">
-                                <form action="#">
-                                    <input type="email" placeholder="Enter Your Email">
-                                    <button><i class="lni lni-envelope"></i></button>
-                                </form>
-                            </div> 
+    <div class="footer_subscribe pt-80">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="subscribe_title mt-45">
+                        <h4 class="title">Suscribete a nuestras novedades</h4>
+                        <p>Recibir novedades semanalmente</p>
+                    </div> 
+                </div>
+                <div class="col-lg-6">
+                    <div class="subscribe_form mt-50">
+                        <form action="#">
+                            <input type="email" placeholder="Ingresa Tu Email">
+                            <button><i class="lni lni-envelope"></i></button>
+                        </form>
+                    </div> 
+                </div>
+            </div> 
+        </div> 
+    </div> 
+    <div class="footer_widget pt-80 pb-130">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 order-md-1 order-lg-1">
+                    <div class="footer_about mt-45">
+                        <h4 class="footer_title">Animalia</h4>
+                        <p>Animalia Express, Animalia Rural, Animalia Web, Animalia Express, Animalia Rural, Animalia Web.</p>
+                        <ul class="social">
+                            <li><a href="https://www.facebook.com/Animalia-110563507299462"><i class="lni lni-facebook-filled"></i></a></li>
+                            <!--<li><a href="#"><i class="lni lni-twitter-original"></i></a></li>-->
+                            <li><a href="https://www.instagram.com/animalia_arica/"><i class="lni lni-instagram-original"></i></a></li>
+                            <!--<li><a href="#"><i class="lni lni-linkedin-original"></i></a></li>-->
+                        </ul>
+                    </div> 
+                </div>
+                <div class="col-lg-4 order-md-3 order-lg-2">
+                    <div class="footer_link_wrapper d-flex flex-wrap">
+                        <div class="footer_link mt-45">
+                            <h4 class="footer_title">Horarios de Atención</h4>
+                            <ul>
+                                <li>Lunes - Viernes: 09.00 A.M - 09.00 P.M</li>
+                                <li>Sábados: 10.00 A.M - 08.00 P.M</li>
+                                <li>Domingos: 10.00 A.M - 03.00 P.M</li>
+                                <!--                                <li>Half-Holidays: 08.00 A.M - 02.00 P.M</li>
+                                                                <li>Twe: 08.00 A.M - 02.00 P.M</li>-->
+                            </ul>
+                        </div> 
+                    </div> 
+                </div>
+                <div class="col-lg-4 col-md-4 order-md-2 order-lg-3">
+                    <div class="footer_instagram mt-45">
+                        <h4 class="footer_title">Instagram</h4>
+                        <div class="instagram_image">
+                            <a href="#"><img src="<?= $assets; ?>images/instagram-1.jpg" alt="instagram"></a>
+                            <a href="#"><img src="<?= $assets; ?>images/instagram-2.jpg" alt="instagram"></a>
+                            <a href="#"><img src="<?= $assets; ?>images/instagram-3.jpg" alt="instagram"></a>
+                            <a href="#"><img src="<?= $assets; ?>images/instagram-4.jpg" alt="instagram"></a>
+                            <a href="#"><img src="<?= $assets; ?>images/instagram-4.jpg" alt="instagram"></a>
+                            <a href="#"><img src="<?= $assets; ?>images/instagram-3.jpg" alt="instagram"></a>
+                            <a href="#"><img src="<?= $assets; ?>images/instagram-2.jpg" alt="instagram"></a>
+                            <a href="#"><img src="<?= $assets; ?>images/instagram-1.jpg" alt="instagram"></a>
                         </div>
                     </div> 
-                </div> 
+                </div>
             </div> 
-            <div class="footer_widget pt-80 pb-130">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-4 order-md-1 order-lg-1">
-                            <div class="footer_about mt-45">
-                                <h4 class="footer_title">About Us</h4>
-                                <p>Lorem ipsum dolor sit amet, csadipscing elitr, sed diam nonumy eirmod teeinidunt ut labore et dolore magna aliquam erat, sed diam voluptua. At vero.</p>
-                                <ul class="social">
-                                    <li><a href="#"><i class="lni lni-facebook-filled"></i></a></li>
-                                    <li><a href="#"><i class="lni lni-twitter-original"></i></a></li>
-                                    <li><a href="#"><i class="lni lni-instagram-original"></i></a></li>
-                                    <li><a href="#"><i class="lni lni-linkedin-original"></i></a></li>
-                                </ul>
-                            </div> 
-                        </div>
-                        <div class="col-lg-4 order-md-3 order-lg-2">
-                            <div class="footer_link_wrapper d-flex flex-wrap">
-                                <div class="footer_link mt-45">
-                                    <h4 class="footer_title">Opening Hours</h4>
-                                    <ul>
-                                        <li>Mon-Fri: 08.00 A.M - 10.00 P.M</li>
-                                        <li>Saturday: 08.00 A.M - 02.00 P.M</li>
-                                        <li>Sunday: Closed</li>
-                                        <li>Half-Holidays: 08.00 A.M - 02.00 P.M</li>
-                                        <li>Twe: 08.00 A.M - 02.00 P.M</li>
-                                    </ul>
-                                </div> 
-                            </div> 
-                        </div>
-                        <div class="col-lg-4 col-md-4 order-md-2 order-lg-3">
-                            <div class="footer_instagram mt-45">
-                                <h4 class="footer_title">Instagram Feed</h4>
-                                <div class="instagram_image">
-                                    <a href="#"><img src="assets/images/instagram-1.jpg" alt="instagram"></a>
-                                    <a href="#"><img src="assets/images/instagram-2.jpg" alt="instagram"></a>
-                                    <a href="#"><img src="assets/images/instagram-3.jpg" alt="instagram"></a>
-                                    <a href="#"><img src="assets/images/instagram-4.jpg" alt="instagram"></a>
-                                    <a href="#"><img src="assets/images/instagram-4.jpg" alt="instagram"></a>
-                                    <a href="#"><img src="assets/images/instagram-3.jpg" alt="instagram"></a>
-                                    <a href="#"><img src="assets/images/instagram-2.jpg" alt="instagram"></a>
-                                    <a href="#"><img src="assets/images/instagram-1.jpg" alt="instagram"></a>
-                                </div>
-                            </div> 
-                        </div>
-                    </div> 
-                </div> 
+        </div> 
+    </div> 
+    <div class="footer_copyright">
+        <div class="container">
+            <div class="copyright text-center">
+                <p>Desarrollado por <a href="https://gambessit.cl" rel="nofollow">GambesS IT - CHILE</a></p>
             </div> 
-            <div class="footer_copyright">
-                <div class="container">
-                    <div class="copyright text-center">
-                        <p>Designed and Developed by <a href="https://uideck.com" rel="nofollow">UIdeck</a></p>
-                    </div> 
-                </div> 
-            </div> 
-            <div class="footer_shape">
-                <img src="assets/images/footer_shape.png" alt="footer shape">
-            </div> 
-        </section>
+        </div> 
+    </div> 
+    <div class="footer_shape">
+        <img src="assets/images/footer_shape.png" alt="footer shape">
+    </div> 
+</section>
 <a href="#" class="back-to-top"><i class="lni lni-chevron-up"></i></a>
 
 
@@ -88,7 +88,7 @@
             <div class="col-md-6 col-sm-12">
                 <div class="title-footer"><span><?= lang('about_us'); ?></span></div>
                 <p>
-                    <?= $shop_settings->description; ?> <a href="<?= site_url('page/' . $shop_settings->about_link); ?>"><?= lang('read_more'); ?></a>
+<?= $shop_settings->description; ?> <a href="<?= site_url('page/' . $shop_settings->about_link); ?>"><?= lang('read_more'); ?></a>
                 </p>
                 <p>
                     <i class="fa fa-phone"></i> <span class="margin-left-md"><?= $shop_settings->phone; ?></span>
@@ -112,15 +112,15 @@
                 <div class="title-footer"><span><?= lang('follow_us'); ?></span></div>
                 <p><?= $shop_settings->follow_text; ?></p>
                 <ul class="follow-us">
-                    <?php if (!empty($shop_settings->facebook)) { ?>
-                        <li><a target="_blank" href="<?= $shop_settings->facebook; ?>"><i class="fa fa-facebook"></i></a></li>
-                    <?php } if (!empty($shop_settings->twitter)) { ?>
-                        <li><a target="_blank" href="<?= $shop_settings->twitter; ?>"><i class="fa fa-twitter"></i></a></li>
-                    <?php } if (!empty($shop_settings->google_plus)) { ?>
-                        <li><a target="_blank" href="<?= $shop_settings->google_plus; ?>"><i class="fa fa-google-plus"></i></a></li>
-                    <?php } if (!empty($shop_settings->instagram)) { ?>
-                        <li><a target="_blank" href="<?= $shop_settings->instagram; ?>"><i class="fa fa-instagram"></i></a></li>
-                    <?php } ?>
+<?php if (!empty($shop_settings->facebook)) { ?>
+                            <li><a target="_blank" href="<?= $shop_settings->facebook; ?>"><i class="fa fa-facebook"></i></a></li>
+<?php } if (!empty($shop_settings->twitter)) { ?>
+                            <li><a target="_blank" href="<?= $shop_settings->twitter; ?>"><i class="fa fa-twitter"></i></a></li>
+<?php } if (!empty($shop_settings->google_plus)) { ?>
+                            <li><a target="_blank" href="<?= $shop_settings->google_plus; ?>"><i class="fa fa-google-plus"></i></a></li>
+<?php } if (!empty($shop_settings->instagram)) { ?>
+                            <li><a target="_blank" href="<?= $shop_settings->instagram; ?>"><i class="fa fa-instagram"></i></a></li>
+<?php } ?>
                 </ul>
             </div>
 
@@ -241,21 +241,21 @@
 
 <?php if ($m == 'shop' && $v == 'product') { ?>
     <script type="text/javascript">
-        $(document).ready(function ($) {
-            $('.rrssb-buttons').rrssb({
-                title: '<?= $product->code . ' - ' . $product->name; ?>',
-                url: '<?= site_url('product/' . $product->slug); ?>',
-                image: '<?= base_url('assets/uploads/' . $product->image); ?>',
-                description: '<?= $page_desc; ?>',
-                // emailSubject: '',
-                // emailBody: '',
-            });
+    $(document).ready(function ($) {
+        $('.rrssb-buttons').rrssb({
+            title: '<?= $product->code . ' - ' . $product->name; ?>',
+            url: '<?= site_url('product/' . $product->slug); ?>',
+            image: '<?= base_url('assets/uploads/' . $product->image); ?>',
+            description: '<?= $page_desc; ?>',
+            // emailSubject: '',
+            // emailBody: '',
         });
+    });
     </script>
 <?php } ?>
 <?php if ($message || $warning || $error || $reminder) { ?>
-<script type="text/javascript">
-$(document).ready(function() {
+    <script type="text/javascript">
+        $(document).ready(function () {
     <?php if ($message) { ?>
                 sa_alert('<?= lang('success'); ?>', '<?= trim(str_replace(array("\r", "\n", "\r\n"), '', addslashes($message))); ?>');
     <?php } if ($warning) { ?>
@@ -266,8 +266,8 @@ $(document).ready(function() {
                 sa_alert('<?= lang('reminder'); ?>', '<?= trim(str_replace(array("\r", "\n", "\r\n"), '', addslashes($reminder))); ?>', 'info');
     <?php } ?>
 
-});
-</script>
+        });
+    </script>
 <?php } ?>
 </body>
 </html>
