e26ee1ce5926a94dbc8e5ff3e86ee7fb34010f8b
G4MB3SS <razielvalle@gambess.com>
2020-11-28 16:15:17 -0300

    Raziel Valle Miranda: Añadiendo footer coffee sin modificar

--- themes/default/shop/views/footer.php
+++ themes/default/shop/views/footer.php
@@ -1,5 +1,88 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<section class="footer">
+<section id="footer" class="footer_area">
+            <div class="footer_subscribe pt-80">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6">
+                            <div class="subscribe_title mt-45">
+                                <h4 class="title">Subscribe Our Newsletter</h4>
+                                <p>To recieve monthly updates</p>
+                            </div> 
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="subscribe_form mt-50">
+                                <form action="#">
+                                    <input type="email" placeholder="Enter Your Email">
+                                    <button><i class="lni lni-envelope"></i></button>
+                                </form>
+                            </div> 
+                        </div>
+                    </div> 
+                </div> 
+            </div> 
+            <div class="footer_widget pt-80 pb-130">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 order-md-1 order-lg-1">
+                            <div class="footer_about mt-45">
+                                <h4 class="footer_title">About Us</h4>
+                                <p>Lorem ipsum dolor sit amet, csadipscing elitr, sed diam nonumy eirmod teeinidunt ut labore et dolore magna aliquam erat, sed diam voluptua. At vero.</p>
+                                <ul class="social">
+                                    <li><a href="#"><i class="lni lni-facebook-filled"></i></a></li>
+                                    <li><a href="#"><i class="lni lni-twitter-original"></i></a></li>
+                                    <li><a href="#"><i class="lni lni-instagram-original"></i></a></li>
+                                    <li><a href="#"><i class="lni lni-linkedin-original"></i></a></li>
+                                </ul>
+                            </div> 
+                        </div>
+                        <div class="col-lg-4 order-md-3 order-lg-2">
+                            <div class="footer_link_wrapper d-flex flex-wrap">
+                                <div class="footer_link mt-45">
+                                    <h4 class="footer_title">Opening Hours</h4>
+                                    <ul>
+                                        <li>Mon-Fri: 08.00 A.M - 10.00 P.M</li>
+                                        <li>Saturday: 08.00 A.M - 02.00 P.M</li>
+                                        <li>Sunday: Closed</li>
+                                        <li>Half-Holidays: 08.00 A.M - 02.00 P.M</li>
+                                        <li>Twe: 08.00 A.M - 02.00 P.M</li>
+                                    </ul>
+                                </div> 
+                            </div> 
+                        </div>
+                        <div class="col-lg-4 col-md-4 order-md-2 order-lg-3">
+                            <div class="footer_instagram mt-45">
+                                <h4 class="footer_title">Instagram Feed</h4>
+                                <div class="instagram_image">
+                                    <a href="#"><img src="assets/images/instagram-1.jpg" alt="instagram"></a>
+                                    <a href="#"><img src="assets/images/instagram-2.jpg" alt="instagram"></a>
+                                    <a href="#"><img src="assets/images/instagram-3.jpg" alt="instagram"></a>
+                                    <a href="#"><img src="assets/images/instagram-4.jpg" alt="instagram"></a>
+                                    <a href="#"><img src="assets/images/instagram-4.jpg" alt="instagram"></a>
+                                    <a href="#"><img src="assets/images/instagram-3.jpg" alt="instagram"></a>
+                                    <a href="#"><img src="assets/images/instagram-2.jpg" alt="instagram"></a>
+                                    <a href="#"><img src="assets/images/instagram-1.jpg" alt="instagram"></a>
+                                </div>
+                            </div> 
+                        </div>
+                    </div> 
+                </div> 
+            </div> 
+            <div class="footer_copyright">
+                <div class="container">
+                    <div class="copyright text-center">
+                        <p>Designed and Developed by <a href="https://uideck.com" rel="nofollow">UIdeck</a></p>
+                    </div> 
+                </div> 
+            </div> 
+            <div class="footer_shape">
+                <img src="assets/images/footer_shape.png" alt="footer shape">
+            </div> 
+        </section>
+<a href="#" class="back-to-top"><i class="lni lni-chevron-up"></i></a>
+
+
+
+<!--<section class="footer">
     <div class="container padding-bottom-md">
         <div class="row">
             <div class="col-md-6 col-sm-12">
@@ -86,12 +169,11 @@
             <div class="clearfix"></div>
         </div>
     </div>
-</section>
+</section>-->
 
-<a href="#" class="back-to-top text-center" onclick="$('body,html').animate({scrollTop: 0}, 500); return false">
+<!--<a href="#" class="back-to-top text-center" onclick="$('body,html').animate({scrollTop: 0}, 500); return false">
     <i class="fa fa-angle-double-up"></i>
-</a>
-</section>
+</a>-->
 <?php if (!get_cookie('shop_use_cookie') && get_cookie('shop_use_cookie') != 'accepted' && !empty($shop_settings->cookie_message)) { ?>
     <div class="cookie-warning">
         <div class="bounceInLeft alert alert-info">
@@ -142,12 +224,21 @@
 </script>
 <script src="<?= $assets; ?>js/vendor/jquery-3.5.1.min.js"></script>
 <script src="<?= $assets; ?>js/vendor/modernizr-3.7.1.min.js"></script>
-<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
+<script src="<?= $assets; ?>js/popper.min.js" ></script>
 <script src="<?= $assets; ?>js/bootstrap.min.js"></script>
+<script src="<?= $assets; ?>js/slick.min.js" type="text/javascript"></script>
+<script src="<?= $assets; ?>js/ajax-contact.js" type="text/javascript"></script>
+<script src="<?= $assets; ?>js/jquery.easing.min.js" type="text/javascript"></script>
+<script src="<?= $assets; ?>js/scrolling-nav.js" type="text/javascript"></script>
+<script src="<?= $assets; ?>js/wow.min.js" type="text/javascript"></script>
+<!--<script src="<?= $assets; ?>js/main.js" type="text/javascript"></script>-->
 <script src="<?= $assets; ?>js/form_validation.min.js"></script>
 <script src="<?= $assets; ?>js/sticky_kit.min.js"></script>
 <script src="<?= $assets; ?>js/bootstrap_select.min.js"></script>
 <script src="<?= $assets; ?>js/scripts.min.js"></script>
+
+
+
 <?php if ($m == 'shop' && $v == 'product') { ?>
     <script type="text/javascript">
         $(document).ready(function ($) {
